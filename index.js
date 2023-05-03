cd4df1b1fd3e336e5609a0b33f22bb4189b1b8a9
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-05-03 22:13:48 +0600

    bannerDishes loaded

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const bannerDishes = require('./data/bannerDishes.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Recipe Hunter is running')
 });
+
+app.get('/bannerdishes', (req, res) => {
+    res.send(bannerDishes);
+})
 
 app.listen(port, () => {
     console.log(`Recipe Hunter API is running on port: ${port}`)
